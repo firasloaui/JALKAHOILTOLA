09d28cb9b5cab22f88c80972a1ed5861f2098cd3
Firas <firas.louai@stud.pointcollege.fi>
2016-11-30 15:29:36 +0200

    Create Product Groups bottun

---
+++ JALKAHOITLOA/JALKAHOITLOA/Models/ProductGroupsController.cs
@@ -0,0 +1,23 @@
+﻿using JALKAHOITLOA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JALKAHOITLOA.Models
+{
+    public class ProductGroupsController : Controller
+    {
+        // GET: ProductGroups
+        public ActionResult Index()
+        {
+            JalkahoitolaEntities entities = new JalkahoitolaEntities();
+            List<ProductGroup> model = entities.ProductGroups.ToList();
+            entities.Dispose();
+
+            return View(model);
+            
+        }
+    }
+}

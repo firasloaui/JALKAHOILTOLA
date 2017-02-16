d77ec124b059110b082a8f1047ed5983ac6f5995
Ari Heinonen <ari.heinonen@pointcollege.fi>
2017-02-16 18:42:19 +0200

    Updated database model

--- JALKAHOITLOA/JALKAHOITLOA/Models/Jalkahoitola.Designer.cs
+++ JALKAHOITLOA/JALKAHOITLOA/Models/Jalkahoitola.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\shako\Desktop\JALKAHOILTOLA\Marko\asas\JALKAHOITLOA\JALKAHOITLOA\Models\Jalkahoitola.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\heinoar\Source\Repos\JALKAHOILTOLA\JALKAHOITLOA\JALKAHOITLOA\Models\Jalkahoitola.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
